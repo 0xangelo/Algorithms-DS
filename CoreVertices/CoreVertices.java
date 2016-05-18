aeb9349ebb10f687763dda7263851001385774fb
angelolovatto <angelolovatto@gmail.com>
2016-05-18 13:54:25 -0300

    start

---
+++ CoreVertices/CoreVertices.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Nome: Ângelo Gregório Lovatto
+ * Número USP: 9293435
+ *
+ * Compilação: javac-algs4 
+ * Execução:   java-algs4 
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.*;
+
+public class CoreVertices {
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        int M = StdIn.readInt();
+        Digraph G = new Graph(N);
+        
+        for (int i = 0; i < M; i++) {
+            int v1 = StdIn.readInt();
+            int v2 = StdIn.readInt();
+            G.addEdge(v1, v2);
+        }
+
+    }
+
+}
+
