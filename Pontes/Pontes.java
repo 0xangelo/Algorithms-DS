ff75b7d1f287211cbed93a9105fb60d3e1c61a8e
angelolovatto <angelolovatto@gmail.com>
2016-05-18 13:48:08 -0300

    progresso

--- Pontes/Pontes.java
+++ Pontes/Pontes.java
@@ -11,7 +11,30 @@ import edu.princeton.cs.algs4.*;
 public class Pontes {
 
     public static void main(String[] args) {
-        Graph G = new Graph(stdin);
+        int N = StdIn.readInt();
+        int M = StdIn.readInt();
+        Graph G = new Graph(N);
+        boolean[] marked = new boolean[N];
         
+        for (int i = 0; i < M; i++) {
+            int v1 = StdIn.readInt();
+            int v2 = StdIn.readInt();
+            G.addEdge(v1, v2);
+        }
+
+        Digraph D = new Digraph(N);
+        dfs(0);
+
+        
+    }
+ 
+    private void dfs(int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            D.addEdge(v, w);
+            if (!marked[w]) {
+                dfs(w);
+            }
+        }
     }
 }
