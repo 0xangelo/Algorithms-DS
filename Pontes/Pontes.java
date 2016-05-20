3d033fcc2e2b5db95b93c1ede17e06105e9adfd2
angelolovatto <angelolovatto@gmail.com>
2016-05-20 17:09:08 -0300

    feito

--- Pontes/Pontes.java
+++ Pontes/Pontes.java
@@ -9,12 +9,20 @@
 
 import edu.princeton.cs.algs4.*;
 public class Pontes {
+    static boolean[] marked;
+    static int counter;
+    static int[] preorder;
+    static int[] low;
+ 
 
     public static void main(String[] args) {
         int N = StdIn.readInt();
         int M = StdIn.readInt();
         Graph G = new Graph(N);
-        boolean[] marked = new boolean[N];
+        marked = new boolean[N];
+        counter = 0;
+        preorder = new int[N];
+        low = new int[N];
         
         for (int i = 0; i < M; i++) {
             int v1 = StdIn.readInt();
@@ -22,19 +30,24 @@ public class Pontes {
             G.addEdge(v1, v2);
         }
 
-        Digraph D = new Digraph(N);
-        dfs(0);
-
-        
+        dfs(G, 0, 0);
     }
  
-    private void dfs(int v) {
+    private static void dfs(Graph G, int v, int p) {
         marked[v] = true;
+        preorder[v] = counter++;
+        low[v] = preorder[v];
+        
         for (int w : G.adj(v)) {
-            D.addEdge(v, w);
             if (!marked[w]) {
-                dfs(w);
+                dfs(G, w, v);
+                low[v] = Math.min(low[v], low[w]);
+                if (low[w] == preorder[w])
+                    StdOut.println(v + " " + w);
             }
+            else if (w != p)
+                low[v] = Math.min(low[v], low[w]);
+            
         }
     }
 }
