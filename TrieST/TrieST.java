9e2cf3ab918a0d86490c3e292da0381ed5008403
angelolovatto <angelolovatto@gmail.com>
2016-05-31 16:42:31 -0300

    start

---
+++ TrieST/TrieST.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Nome: Ângelo Gregório Lovatto
+ * Número USP: 9293435
+ *
+ * Compilação: javac-algs4 
+ * Execução:   java-algs4 
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.*;
+public class TrieST {
+
+    public static void main(String[] args) {
+
+    }
+
+}
